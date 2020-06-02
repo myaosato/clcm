5e72f169354b3e744d5e6a16a6743886edc5dc8d
myaosato <tetu60u@yahoo.co.jp>
2020-06-02 20:03:45 +0900

    add attach-thematic-break!?

--- src/nodes/thematic-break.lisp
+++ src/nodes/thematic-break.lisp
@@ -4,7 +4,8 @@
   (:import-from :cl-ppcre
                 :scan)
   (:export :thematic-break-node
-           :is-thematic-break-line))
+           :is-thematic-break-line
+           :attach-thematic-break!?))
 (in-package :clcm/nodes/thematic-break)
 
 ;;
@@ -26,3 +27,10 @@
   (or (scan "^ {0,3}(?:\\*\\s*){3,}$" line)
       (scan "^ {0,3}(?:_\\s*){3,}$" line)
       (scan "^ {0,3}(?:-\\s*){3,}$" line)))
+
+;;
+(defun attach-thematic-break!? (node line)
+  (when (is-thematic-break-line line)
+    (let ((child (make-instance 'thematic-break-node :is-open nil)))
+      (add-child node child)
+      child)))
