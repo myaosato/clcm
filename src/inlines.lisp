0d93175ae086e54a16f40cf7008f0677495a435c
myaosato <tetu60u@yahoo.co.jp>
2023-04-30 17:06:23 +0900

    parser/inlines

--- src/inlines.lisp
+++ src/inlines.lisp
@@ -1,5 +1,7 @@
 (defpackage :clcm/inlines
   (:use :cl)
+  (:import-from :uiop
+                :if-let)
   (:import-from :clcm/code-spans
                 :check-code-spans)
   (:import-from :clcm/autolinks
@@ -15,108 +17,53 @@
                 :check-image-opener
                 :check-link-closer
                 :check-close-link-text)
+  (:import-from :clcm/parser/inlines
+                :make-inline-parser
+                :push-parsed
+                :push-delimiter
+                :search-delimiter
+                :remove-delimiter
+                :update-pointer
+                :inactivate-before
+                :parser-lines
+                :parser-inlines
+                :delimiter-is-active
+                :delimiter-type
+                :delimiter-content)
   (:export :parse-inline))
 (in-package :clcm/inlines)
 
-; delimiter stack
-(defclass delimiter ()
-  ((next :accessor next :initform nil)
-   (prev :accessor prev :initform nil)
-   (is-active :accessor is-active :initform t)
-   (delimiter-type :accessor delimiter-type :initarg :type)
-   (pointer :accessor pointer :initarg :pointer)
-   (number-of :accessor number-of :initarg :number-of :initform 1)
-   (is-opener :accessor is-opener :initarg :is-opener :initform t)
-   (is-closer :accessor is-closer :initarg :is-closer :initform nil)))
-
-(defun push-delimiter (stack type pointer &key (number-of 1) (is-opener t) (is-closer nil))
-  (let ((delimiter (make-instance 'delimiter
-                                  :pointer pointer :type type
-                                  :number-of number-of :is-opener is-opener :is-closer is-closer)))
-    (unless stack
-      (return-from push-delimiter delimiter))
-    (setf (prev delimiter) stack)
-    (setf (next stack) delimiter)
-    delimiter))
-
-(defun search-delimiter (delimiter &rest types)
-  (when delimiter
-    (if (find (delimiter-type delimiter) types)
-        delimiter
-        (apply #'search-delimiter (prev delimiter) types))))
-
-(defun remove-delimiter (delimiter)
-  (let ((prev (prev delimiter))
-        (next (next delimiter)))
-    (when prev (setf (next prev) next))
-    (when next (setf (prev next) prev))))
-
-(defun inactivate-before (delimiter &rest types)
-  (when delimiter
-    (when (find (delimiter-type delimiter) types)
-      (setf (is-active delimiter) nil))
-    (apply #'inactivate-before (prev delimiter) types)))
-
-; inline parser
-(defclass inline-parser ()
-  ((lines :initarg :lines :accessor parser-lines)
-   (inlines :initarg :inlines :accessor parser-inlines)
-   (pointer :initarg :pointer :accessor parser-pointer)
-   (delimter-stack :initarg :delimiter-stack :accessor parser-delimiters)))
-
-(defun make-inline-parser (lines)
-  (let ((inlines (list nil)))
-    (make-instance 'inline-parser
-                   :lines lines
-                   :inlines inlines
-                   :pointer inlines
-                   :delimiter-stack nil)))
-
-(defun push-parsed (parser target)
-  (let ((previous (parser-pointer parser)))
-    (setf (cdr (parser-pointer parser)) (list target))
-    (setf (parser-pointer parser) (cdr (parser-pointer parser)))
-    previous))
-
-(defun push-to-parser-delimiters (parser parsed type &key (number-of 1) (is-opener t) (is-closer nil))
-  (let ((pushed (push-parsed parser parsed)))
-    (setf (parser-delimiters parser)
-          (push-delimiter (parser-delimiters parser) type pushed
-                          :number-of number-of :is-opener is-opener :is-closer is-closer))))
-
 ;; return next position (on lines)
 (defun process-result (parser pos type parsed next)
-  (cond ((eq type :link-opener)
-         (push-to-parser-delimiters parser parsed :link))
-        ((eq type :image-opener)
-         (push-to-parser-delimiters parser parsed :image))
-        ((eq type :link-closer)
-         (let ((opener (search-delimiter (parser-delimiters parser) :link :image)))
-           (cond ((and opener (is-active opener))
-                  (let ((closer (check-link-closer (parser-lines parser) (1+ pos))))
-                    (if closer
-                        (destructuring-bind (_ (dest title) next-pos) closer
-                          (declare (ignore _))
-                          (let ((type (delimiter-type opener))
-                                (content (cddr (pointer opener))))
-                            ;; (pointer opener) := (previous-last "[" "foo" ... ) 
-                            ;;                     or ((previous-last "![" "foo" ... )
-                            (setf (parser-pointer parser) (pointer opener))
-                            (push-parsed parser (list type content dest title))
-                            (remove-delimiter opener)
-                            (when (eq opener (parser-delimiters parser))
-                              (setf (parser-delimiters parser) (prev opener)))
-                            (when (eq type :link)
-                              (inactivate-before opener :link))
-                            (setf next next-pos)))
-                        (push-parsed parser parsed))))
-                 ((and opener (not (is-active opener)))
-                  (remove-delimiter opener)
-                  (when (eq opener (parser-delimiters parser))
-                    (setf (parser-delimiters parser) (prev opener)))
+  (when (eq type :link-opener)
+    (push-delimiter parser parsed :link)
+    (return-from process-result next))
+  (when (eq type :image-opener)
+    (push-delimiter parser parsed :image)
+    (return-from process-result next))
+  (when (eq type :link-closer)
+    (let ((opener (search-delimiter parser :link :image)))
+      (cond ((and opener (delimiter-is-active opener))
+             (let ((closer (check-link-closer (parser-lines parser) (1+ pos))))
+               (if closer
+                   (destructuring-bind (_ (dest title) next-pos) closer
+                     (declare (ignore _))
+                     (let ((type (delimiter-type opener))
+                           (content (delimiter-content opener)))
+                       (update-pointer parser opener)
+                       (push-parsed parser (list type content dest title))
+                       (remove-delimiter parser opener)
+                       (when (eq type :link)
+                         (inactivate-before opener :link))
+                       (setf next next-pos)))
+                   (push-parsed parser parsed))))
+            (opener ;; (not (delimiter-is-active opener)) case
+                  (remove-delimiter parser opener)
                   (push-parsed parser parsed))
-                 (t (push-parsed parser parsed)))))
-        (t (push-parsed parser parsed)))
+            (t
+             (push-parsed parser parsed))))
+    (return-from process-result next))
+  (push-parsed parser parsed)
   next)
 
 (defun parse-inline (lines)
@@ -144,4 +91,4 @@
           :do (incf pos)
           :finally (when (/= pos done)
                      (push-parsed parser (subseq lines done pos))))
-    (values (cdr (parser-inlines parser)) (parser-delimiters parser))))
+    (cdr (parser-inlines parser))))
---
+++ src/parser/inlines.lisp
@@ -0,0 +1,97 @@
+(defpackage :clcm/parser/inlines
+  (:use :cl)
+  (:import-from :uiop
+                :if-let)
+  (:export :make-inline-parser
+           ; parser handlers
+           :push-parsed
+           :push-delimiter
+           :search-delimiter
+           :remove-delimiter
+           :update-pointer
+           :inactivate-before
+           ; parser slots
+           :parser-lines
+           :parser-inlines
+           ; delimiter slots
+           :delimiter-is-active
+           :delimiter-type
+           :delimiter-content))
+(in-package :clcm/parser/inlines)
+
+; delimiter stack
+(defclass delimiter ()
+  ((next           :initform nil                     :accessor next)
+   (prev           :initform nil                     :accessor prev)
+   (is-active      :initform t                       :accessor delimiter-is-active)
+   (delimiter-type :initarg :type                    :accessor delimiter-type)
+   (pointer        :initarg :pointer                 :accessor delimiter-pointer)
+   (number-of      :initarg :number-of :initform 1   :accessor number-of)
+   (is-opener      :initarg :is-opener :initform t   :accessor is-opener)
+   (is-closer      :initarg :is-closer :initform nil :accessor is-closer)))
+
+(defun delimiter-content (delimiter)
+  ;;
+  ;; (delimiter-pointer delimiter) := (previous-last "[" "foo" ... ) 
+  ;;                                  or (previous-last "![" "foo" ... )
+  (cddr (delimiter-pointer delimiter)))
+
+; inline parser
+(defclass inline-parser ()
+  ((lines     :initarg :lines      :accessor parser-lines)
+   (inlines   :initarg :inlines    :accessor parser-inlines)
+   (pointer   :initarg :pointer    :accessor parser-pointer)
+   (delimters :initarg :delimiters :accessor parser-delimiters)))
+
+(defun make-inline-parser (lines)
+  (let ((inlines (list nil)))
+    (make-instance 'inline-parser
+                   :lines lines
+                   :inlines inlines
+                   :pointer inlines
+                   :delimiters nil)))
+
+;; push as parsed
+(defun push-parsed (parser parsed)
+  (let ((previous (parser-pointer parser)))
+    (setf (cdr (parser-pointer parser)) (list parsed))
+    (setf (parser-pointer parser) (cdr (parser-pointer parser)))
+    previous))
+
+;; push as parsed and push as delimiter
+(defun push-delimiter (parser parsed type &key (number-of 1) (is-opener t) (is-closer nil))
+  (let ((delimiter (make-instance 'delimiter
+                                  :pointer (push-parsed parser parsed) :type type
+                                  :number-of number-of :is-opener is-opener :is-closer is-closer))
+        (stack (parser-delimiters parser)))
+    (when stack
+      (setf (prev delimiter) stack)
+      (setf (next stack) delimiter))
+    (setf (parser-delimiters parser) delimiter)))
+
+;; return first found delimiter that has one of specified types.
+(defun search-delimiter (parser &rest types)
+  (if-let (delimiter (parser-delimiters parser))
+    (if (find (delimiter-type delimiter) types)
+        delimiter
+        (apply #'search-delimiter (prev delimiter) types))))
+
+;;
+(defun remove-delimiter (parser delimiter)
+  (let ((prev (prev delimiter))
+        (next (next delimiter)))
+    (when prev (setf (next prev) next))
+    (when next (setf (prev next) prev)))
+  (when (eq delimiter (parser-delimiters parser))
+    (setf (parser-delimiters parser) (prev delimiter))))
+
+;; update parser pointer with delimiter pointer
+(defun update-pointer (parser delimiter)
+  (setf (parser-pointer parser) (delimiter-pointer delimiter)))
+
+;;
+(defun inactivate-before (delimiter &rest types)
+  (when delimiter
+    (when (find (delimiter-type delimiter) types)
+      (setf (delimiter-is-active delimiter) nil))
+    (apply #'inactivate-before (prev delimiter) types)))
